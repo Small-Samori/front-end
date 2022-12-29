150f0371168f7b7c4a3f3d8f62524402e5c9a9fd
Andrews Kangah <andrewsatakangah@gmail.com>
2022-12-29 12:38:06 +0000

    fix: change server ip address

--- src/Services/ApiServices.js
+++ src/Services/ApiServices.js
@@ -1,52 +1,48 @@
-import axios from "axios";
+import axios from 'axios';
 
-const _api =  axios.create({
-    baseURL:'http://3.144.126.31/api',
-    // baseURL:'http://localhost:5000',
-    // headers:{
-    //     "content-type":"application/json",
-    // }
+const _api = axios.create({
+  baseURL: 'http://18.234.79.198/api',
+  // baseURL:'http://localhost:5000',
+  // headers:{
+  //     "content-type":"application/json",
+  // }
 });
 
 const apiService = {
-  
-    postCompound : async (data)=> {
-        try {
-            const result = await _api.post('/compound',data);
-            if(result.status === 200 || result.status === 201){
-                return result?.data;
-            }
+  postCompound: async data => {
+    try {
+      const result = await _api.post('/compound', data);
+      if (result.status === 200 || result.status === 201) {
+        return result?.data;
+      }
 
-            return false;
-        } catch (error) {
-            // console.log('====================================');
-            // console.log(error?.message);
-            // console.log('====================================');
-            return false;
-        }
-    },
+      return false;
+    } catch (error) {
+      // console.log('====================================');
+      // console.log(error?.message);
+      // console.log('====================================');
+      return false;
+    }
+  },
 
-    postFile : async (data)=> {
-        try {
-            const result = await _api.post('/multi',data,{ headers:{
-                'Content-Type': 'multipart/form-data'
-            } });
-            if(result.status === 200 || result.status === 201){
-                return result?.data;
-            }
-            return false;
-        } catch (error) {
-            console.log('====================================');
-            console.log(error?.message);
-            console.log('====================================');
-            return false;
-        }
-    },
+  postFile: async data => {
+    try {
+      const result = await _api.post('/multi', data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      if (result.status === 200 || result.status === 201) {
+        return result?.data;
+      }
+      return false;
+    } catch (error) {
+      console.log('====================================');
+      console.log(error?.message);
+      console.log('====================================');
+      return false;
+    }
+  },
+};
 
-
-}
-
-export {
-    apiService
-}
-
+export { apiService };
